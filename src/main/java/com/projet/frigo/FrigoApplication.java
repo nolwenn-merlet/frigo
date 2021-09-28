883f830c8d76401edfd02a7480df96b4cec58c93
Nolwenn Merlet <nolwenn.merlet@gmail.com>
2021-09-28 11:41:29 -0400

    Creation des fichiers de base avec Spring Initilizr

---
+++ src/main/java/com/projet/frigo/FrigoApplication.java
@@ -0,0 +1,13 @@
+package com.projet.frigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/frigo/FrigoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.frigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
